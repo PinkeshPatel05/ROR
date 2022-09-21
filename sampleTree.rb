bda2c11dbfe78de0dd3f2dad8523e0419fbf9e9b
PinkeshPatel05 <114061096+PinkeshPatel05@users.noreply.github.com>
2022-09-21 14:00:25 +0530

    Add files via upload
    run below commands for the output
    require_relative "sampleTree.rb"
    sampleTree = SampleTree.new
    8.times {sampleTree.grow}
    puts sampleTree

--- sampleTree.rb
+++ sampleTree.rb
@@ -15,31 +15,23 @@ when 2
 @levels.insert(1,"//\\\\")
 else
 prev_leaf_count = @levels[1].length / 2
-#puts @levels[1]
-#puts  prev_leaf_count
-#randomNo = rand(0..1)
-#leaf_count = prev_leaf_count + rand(0..1)
-#puts randomNo
 leaf_count = prev_leaf_count + 1
-#puts  leaf_count
 level=("/" * leaf_count) + ("\\" * leaf_count)
-#puts level
 @levels.insert(1,level)
 end
-#@height +=1
+
 @height = @height+1
 end
 
-def coordinates
-end
+#def coordinates
+#end
 
 def to_s
 str = ""
-#puts level
 @levels.reverse.each do |level|
 padding = @height - level.size / 2
 str += " " * padding + level + "\n"
-#puts  @height
+
 end
 return str
 end
