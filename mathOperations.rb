912766b479ad91023b6ca9a33bf79ad59b61b61c
PinkeshPatel05 <114061096+PinkeshPatel05@users.noreply.github.com>
2022-09-25 14:56:59 +0530

    math operations

---
+++ mathOperations.rb
@@ -0,0 +1,11 @@
+A = [1,2,3,4]
+
+B = [1,2,3] 
+
+C = [1,2,3]
+ D = [4,5,6]
+
+puts "A U B : #{A|B}"
+puts "A N B : #{A&B}"
+puts "A complement B : #{A-B}"
+puts "product() method form : #{C.product(D)}\n\n"
