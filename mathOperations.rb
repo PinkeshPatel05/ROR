f603be2c56e425e80b78b6547fb73486d91939bf
PinkeshPatel05 <114061096+PinkeshPatel05@users.noreply.github.com>
2022-09-26 11:01:30 +0530

    Add files via upload

--- mathOperations.rb
+++ mathOperations.rb
@@ -2,10 +2,7 @@ A = [1,2,3,4]
 
 B = [1,2,3] 
 
-C = [1,2,3]
- D = [4,5,6]
-
 puts "A U B : #{A|B}"
 puts "A N B : #{A&B}"
 puts "A complement B : #{A-B}"
-puts "product() method form : #{C.product(D)}\n\n"
+puts "A Product B : #{A.product(B)}"
