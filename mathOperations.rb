5d4730fa28db5d258039a5295bf0ceb78acf720b
PinkeshPatel05 <114061096+PinkeshPatel05@users.noreply.github.com>
2022-09-27 11:00:35 +0530

    Add files via upload

--- mathOperations.rb
+++ mathOperations.rb
@@ -2,7 +2,46 @@ A = [1,2,3,4]
 
 B = [1,2,3] 
 
-puts "A U B : #{A|B}"
-puts "A N B : #{A&B}"
-puts "A complement B : #{A-B}"
+C = [5,6,7]
+U = [1,2,3,4,5,6,7]
+aORb = A|B
+aAndB = A&B
+aComplB = A-B
+bORc = B|C
+
+
+# puts "A U B : #{A|B}"
+# puts "A N B : #{A&B}"
+puts "A U B : #{aORb}"
+puts "A N B : #{aAndB}"
+puts "A complement B : #{aComplB}"
 puts "A Product B : #{A.product(B)}"
+
+#Commutative Law
+#This law states that no matter in which order we use the variables. It means that the order of variables doesn't matter
+#A+B = B+A
+bORa = B|A
+if(aORb==bORa)
+    puts "Commutative Law Proved"
+end
+
+#Associative Law
+#A + (B + C) = (A + B) + C
+abc1 = A|bORc
+abc2 = aORb|C
+if(abc1==abc2)
+    puts "Associative Law Proved"
+end
+
+#De Morgan Law
+#The operation of an OR and AND logic circuit will remain same if we invert all the inputs, change operators from AND to OR and OR to AND, and invert the output.
+#(A+B)' = A'.B'
+lhs = U - aORb
+
+aCompl = U - A
+bcompl = U - B
+rhs = aCompl&bcompl
+
+if(lhs==rhs)
+    puts "De Morgan's Law Proved"
+end
